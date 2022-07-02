7ba9874f905b11165292e33cc5c6c53c43047b95
Nicolas Ribeiro <nicolas.ribeiro.s@outlook.com>
2022-07-02 14:25:43 -0300

    refactor: support multiple sub commands

--- src/classes/ExecutorManager.ts
+++ src/classes/ExecutorManager.ts
@@ -1,4 +1,4 @@
-import { APIApplicationCommandInteraction, APIContextMenuInteraction, ApplicationCommandType } from 'discord-api-types/v10';
+import { APIApplicationCommandInteraction, APIContextMenuInteraction, ApplicationCommandOptionType, ApplicationCommandType } from 'discord-api-types/v10';
 import type { BaseCommandExecuteOptions } from './Command';
 import type { Executor } from './Executor';
 
@@ -24,9 +24,19 @@ class ExecutorManager {
   retreive(data: APIApplicationCommandInteraction) {
     const { name, type } = data.data;
 
+    let commandName = name;
+
+    if (data.data.type === ApplicationCommandType.ChatInput) {
+      const subcommandName = data.data.options?.find(x => x.type === ApplicationCommandOptionType.Subcommand)?.name;
+
+      if (data.data.options?.find(x => x.type === ApplicationCommandOptionType.Subcommand)) {
+        commandName = `${name} ${subcommandName}`;
+      }
+    }
+
     switch (type) {
       case ApplicationCommandType.ChatInput:
-        return this.get(name, 'slash');
+        return this.get(commandName, 'slash');
       case ApplicationCommandType.User:
         return this.get(name, 'user');
       case ApplicationCommandType.Message:
